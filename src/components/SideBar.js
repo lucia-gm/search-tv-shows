24a16b275aa49da8408b61140fbf01442af39bdb
Lucia Gonzalez Moscoso <luciagonzalezmoscoso@gmail.com>
2018-10-11 18:01:07 -0700

    feat: Hide sidebar

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const SideBar = (props) => (
-  <div className="sidebar">
-    <input
-      className="search"
-      autoFocus={true}
-      type="text"
-      placeholder="Search by title"
-      onChange={event => props.updateSearch(event.target.value)}
-    />
-  </div>
-)
+class SideBar extends Component{
+  onMenuClick = (event) => {
+    event.preventDefault();
+    const menuIcon = document.getElementById('burger-icon');
+    menuIcon.classList.toggle('open');
+    menuIcon.nextSibling.classList.toggle('open');
+  }
+
+  render() {
+    return (
+      <div className="sidebar">
+        <div id="burger-icon" className="open" onClick={this.onMenuClick}>
+          <div className="burger-icon-bar1"></div>
+          <div className="burger-icon-bar2"></div>
+          <div className="burger-icon-bar3"></div>
+        </div>
+        <div className="menu open">
+          <input
+            className="search"
+            autoFocus={true}
+            type="text"
+            placeholder="Search by title"
+            onChange={event => this.props.updateSearch(event.target.value)}
+          />
+        </div>
+      </div>
+    )
+  }
+}
 
 export default SideBar;
